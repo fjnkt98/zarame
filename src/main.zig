5f42fa00df87a02cdf47faec4b6b76bf77e53236
fjnkt98 <fjnkt98@gmail.com>
2026-03-22 16:10:12 +0900

    upgrade to zig 0.15.2

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,19 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buffer: [1024]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buffer);
+    const stdout = &writer.interface;
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+    try stdout.flush();
 }
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
+    const allocator = std.testing.allocator;
+    var list = try std.ArrayList(i32).initCapacity(allocator, 1);
+    defer list.deinit(allocator); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(allocator, 42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
 
