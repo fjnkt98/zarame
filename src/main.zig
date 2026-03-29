90940ab8c5bbdc881b2aaa3cd609d790d0fb7f38
fjnkt98 <fjnkt98@gmail.com>
2026-03-29 12:41:58 +0900

    organize unnecessary codes

--- src/main.zig
+++ src/main.zig
@@ -9,30 +9,6 @@ pub fn main() !void {
     const stdout = &writer.interface;
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    try stdout.print("2 + 3 = {d}\n", .{lib.add(2, 3)});
 
     try stdout.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    const allocator = std.testing.allocator;
-    var list = try std.ArrayList(i32).initCapacity(allocator, 1);
-    defer list.deinit(allocator); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(allocator, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
